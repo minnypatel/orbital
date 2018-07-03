d6f976bbd690642d0f82ca2af388d9635d9a6f76
minnypatel <jasmin.patel@sky.uk>
2018-07-03 13:12:16 +0100

    tidied up js script

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ console.log('script is here!');
 
 // init part, set your own values
 var sat = {
-    elt: null
-    ,a: 0         // in radian
-    ,r: 100         // radius
-    ,da: 0.05     // in radian
-    ,x: 0
-    ,y: 0
+    elt: null,
+    a: 0,         // in radian
+    r: 100,       // radius
+    da: 0.05,     // in radian
+    x: 0,
+    y: 0,
     // Center is actualy center (100, 100) minus
     // half the size of the orbiting object 15x15
-    ,center: { x: (100 - 15), y: (100 - 15) }
-}
+    center: { x: (100 - 15), y: (100 - 15) }
+};
 
-sat.move = function(){
+sat.move = function() {
     // each modification
     this.a += this.da
     this.x = this.center.x + (this.r * Math.sin(this.a));
@@ -22,7 +22,7 @@ sat.move = function(){
     //console.log(this.x, this.y);
     this.elt.style.top = this.y + "px";
     this.elt.style.left = this.x + "px";
-}
+};
 
 sat.elt = document.getElementById('sat1');
 
